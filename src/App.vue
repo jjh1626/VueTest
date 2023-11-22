621e3180a2c6eae132f81c8f0324428713e34fef
hong <hong@naver.com>
2023-11-23 00:38:52 +0900

    todo 추가

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,70 @@
 <template>
-  <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
+  <div id="app" class="container">
+    <h1 class="text-center">Todo App</h1>
+    <input 
+      v-model="todoText"
+      type="text" 
+      class="w-100 p-2" 
+      placeholder="Type todo"
+      @keyup.enter="addItem"
+    >
+    <hr>
+    <TodoComp 
+        v-for="todo in todos" 
+        :key="todo.id"
+        :todoPs="todo"
+        @toggle-checkbox="toggleCheckbox"
+        @click-delete="deleteTodo"
+    />
   </div>
 </template>
 
+<script>
+import TodoComp from '@/components/TodoComp.vue';
+
+export default {
+  components: {
+    TodoComp
+  },
+  data(){
+    return {
+      todoText:'',
+      todos: [
+        {id:1, text:'buy a car', checked:false},
+        {id:2, text:'play game', checked:false},
+      ]
+    }
+  },
+  methods: {
+    addItem(e){
+      console.log(e.target.value);
+      this.todos.push({
+        id: Math.random(),
+        text: e.target.value,
+        checked: false
+      });
+      this.todoText = '';
+    },
+    toggleCheckbox({id, checked}){
+      //console.log(id, checked);
+      const index = this.todos.findIndex(todo => {
+        return todo.id === id;
+      });
+      this.todos[index].checked = checked;
+    },
+    deleteTodo(id){
+      /* splice 사용한 방법
+      const index = this.todos.findIndex(todo => {
+        return todo.id === id;
+      });
+      this.todos.splice(index,1); */
+      // filter 사용한 방법
+      this.todos = this.todos.filter(todo => todo.id !== id);
+    }
+  }
+}
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
---
+++ src/components/TodoComp.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="mb-2">
+        <input 
+            type="checkbox" 
+            :checked="todoPs.checked"
+            @change="toggleCheckbox">
+        <span 
+            class="m-3 flex-grow-1"
+            :class="todoPs.checked ? 'text-muted' : ''"
+            :style="todoPs.checked ? 'text-decoration:line-through' : ''">
+            {{ todoPs.text }}
+        </span>
+        <button 
+            class="btn btn-danger btn-sm"
+            @click="clickDelete">Delete</button>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        todoPs: {
+            type: Object,
+            require: true
+        }
+    },
+    methods:{
+        toggleCheckbox(e){
+            this.$emit('toggle-checkbox',{
+                id: this.todoPs.id,
+                checked: e.target.checked
+            })
+        },
+        clickDelete(){
+            this.$emit('click-delete', this.todoPs.id);
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
